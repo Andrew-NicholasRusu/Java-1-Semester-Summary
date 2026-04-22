776707225177482ef16e3d6bfc6712f941c6aa5f
Andrew-Nicholas Rusu <andrewnicholasrusu@gmail.com>
2026-04-22 15:29:43 -0400

    Delete Tutor_Work_&_Practices/Champlain_College_Tutorials/WednesdayTutorial_#1_Winter2026 directory

--- Tutor_Work_&_Practices/Champlain_College_Tutorials/WednesdayTutorial_#1_Winter2026/src/main/java/VehicleInfo.java
+++
@@ -1,7 +0,0 @@
-// 5.1 Interface: VehicleInfo:
-
-public interface VehicleInfo {
-    // Create an interface named VehicleInfo that defines the following methods:
-    public void displayInfo(); // Displays vehicle details
-    public void startEngine(); // Describes how the vehicle starts
-}
